865af06965e5eaea0f546d056be2fc0fde1c5d67
Arne Govaerts <arne.govaerts@student.hogent.be>
2022-03-08 20:59:51 +0100

    passwordless login

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 from src.auth import Auth
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Cookie
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -15,27 +15,36 @@ app = FastAPI()
 app.mount('/static', StaticFiles(directory='dist'), name='dist')
 templates = Jinja2Templates(directory='templates')
 
+# ----------------------------------------------------------------------------- #
+# Infosite                                                                      #
+# ----------------------------------------------------------------------------- #
 @app.get('/')
 async def root(request: Request):
     return templates.TemplateResponse('landing.html', {'request': request})
 
+# ----------------------------------------------------------------------------- #
+# Passwordless inloggen                                                         #
+# ----------------------------------------------------------------------------- #
 @app.get('/aanmelden')
 async def login(
         request: Request,
         demo: Optional[bool] = False,
-        token: Optional[str] = '',
-        email: Optional[str] = '',
         verzonden: Optional[bool] = False,
-        error: Optional[bool] = False
+        error: Optional[bool] = False,
+        _el_au: Optional[str] = Cookie('')
     ):
+    auth = Auth(request)
+
+    # check cookie
+    if _el_au != '':
+        veri = auth.verify_token(_el_au)
+        if veri['verified']:
+            return RedirectResponse('/dashboard', 303)
+    
     if demo:
-        # set cookie for demo user and redirect
-        return RedirectResponse('/dashboard', 303)
-    elif token and email:
-        auth = Auth(request)
-        res = auth.validate_token(token, email)
-        # set cookie for user and redirect
-        return RedirectResponse('/dashboard', 303)
+        resp = RedirectResponse('/dashboard', 303)
+        resp.set_cookie('_el_au', 'demo', max_age=86400, path='/', httponly=True)
+        return resp
     elif verzonden:
         return templates.TemplateResponse('auth/send.html', {'request': request, 'error': error})
     else:
@@ -48,8 +57,42 @@ async def do_login(request: Request):
     
     auth = Auth(request)
     send = auth.send_token(email)
-    return RedirectResponse('/aanmelden?verzonden=true&error=' + str(not send), 303)
+    return RedirectResponse('/aanmelden?verzonden=true&error=' + str(int(not send)), 303)
 
+@app.get('/verifieren/{token}')
+async def verifieren(request: Request, token: str):
+    auth = Auth(request)
+    veri = auth.verify_token(token, 10)
+
+    if veri['verified']:
+        resp = RedirectResponse('/dashboard', 303)
+        resp.set_cookie('_el_au', veri['token'], max_age=86400, path='/', httponly=True)
+        return resp
+    else:
+        return RedirectResponse('/aanmelden', 303)
+
+# ----------------------------------------------------------------------------- #
+# Dashboard                                                                     #
+# ----------------------------------------------------------------------------- #
+@app.get('/uitloggen')
+async def uitloggen(request: Request, _el_au: Optional[str] = Cookie('')):
+    auth = Auth(request)
+    veri = auth.verify_token(_el_au)
+
+    resp = RedirectResponse('/', 303)
+    if veri['verified']:
+        resp.set_cookie('_el_au', veri['token'], max_age=0, path='/', httponly=True)
+    return resp
+
+# ----------------------------------------------------------------------------- #
+# Dashboard                                                                     #
+# ----------------------------------------------------------------------------- #
 @app.get('/dashboard')
-async def dashboard(request: Request):
+async def dashboard(request: Request, _el_au: Optional[str] = Cookie('')):
+    auth = Auth(request)
+    veri = auth.verify_token(_el_au)
+    
+    if not veri['verified']:
+        return RedirectResponse('/aanmelden', 303)
+    
     return templates.TemplateResponse('app/dashboard.html', {'request': request})
--- src/auth.py
+++ src/auth.py
@@ -8,7 +8,7 @@ class Auth:
         self.secret = os.getenv('AUTH_SECRET')
         
         self.email_metadata = {
-            'smtp-server':  os.getenv('AUTH_SMTP_SERVER'),
+            'smtp_server':  os.getenv('AUTH_SMTP_SERVER'),
             'port':         os.getenv('AUTH_SMTP_PORT'),
             'username':     os.getenv('AUTH_SMTP_USERNAME'),
             'password':     os.getenv('AUTH_SMTP_PASSWORD'),
@@ -25,56 +25,70 @@ class Auth:
         )
 
     def send_token(self, email) -> bool:
-        token = secrets.token_urlsafe(16)
-
         try:
             with queries.Session(self.db_connection_uri) as session:
-                results = session.query('''create table if not exists token_auth (
-                    token           char(24) not null unique primary key,
-                    email           varchar(254) not null,
-                    valid_until     timestamp
+                # tabellen moet bestaan
+                results = session.query('''create table if not exists users (
+                    user_id                 int primary key generated always as identity,
+                    email                   varchar(254) not null unique,
+                    user_created_on         timestamp not null,
+                    last_token              text unique,
+                    last_token_created_on   timestamp,
+                    last_token_verified_on  timestamp
                 )
                 ''')
-                if results:
-                    results = session.query("insert into token_auth values (%s, %s, now() + INTERVAL '" + str(self.token_ttl) + " min')", [token, email])
 
-                    if results:
-                        message = 'From: {}\nSubject: {}\n\nHallo.\n\nJe vroeg een aanmeldlink aan (request van {}).\nGebruik volgende url om aan te melden: http://127.0.0.1:8000/aanmelden?token={}&email={}\n\nTeam ElderSpeaky'.format(
-                            self.email_metadata['email'],
-                            self.email_metadata['subject'],
-                            self.client_ip,
-                            token,
-                            email
-                        )
-            
-                        server = smtplib.SMTP(self.email_metadata['smtp-server'], self.email_metadata['port'])
-                        server.ehlo()
-                        server.starttls()
-                        server.login(self.email_metadata['username'], self.email_metadata['password'])
-                        server.sendmail(self.email_metadata['email'], email, message)
-                        server.close()
-                        
-                        return True
-                    else:
-                        # error
-                        return False
-                else:
-                    # error
-                    return False
+                # als user, bestaat, genereer nieuwe token
+                new_token = session.query('update users set last_token = md5(now() || %s), last_token_created_on = now() where email = %s returning last_token', [email, email])
+
+                if len(new_token) == 0:
+                     # user bestaat niet, maak user en stuur verificatie
+                    new_token = session.query('insert into users (email, user_created_on, last_token, last_token_created_on) values(%s, now(), md5(now() || %s), now()) returning last_token', [email, email])
+
+                new_token = new_token.as_dict()
+                
+                message = 'From: {}\nSubject: {}\n\nHey!\n\nWe registeerden een aanmeldpoging van {}.\nOm je aanmelding te bevestigen, ga naar {}.\n\nMet vriendelijke groeten\nTeam ElderSpeaky'.format(
+                    self.email_metadata['email'],
+                    self.email_metadata['subject'],
+                    self.client_ip,
+                    'http://127.0.1:8000/verifieren/' + new_token['last_token']
+                )
+                server = smtplib.SMTP(self.email_metadata["smtp_server"], self.email_metadata["port"])
+                server.ehlo()
+                server.starttls()
+                server.login(self.email_metadata["username"], self.email_metadata["password"])
+                server.sendmail(self.email_metadata['email'], email, message)
+                server.close()
+
+                return True
         except Exception as e:
             # log error
             print(str(e))
             return False
 
-    def validate_token(self, return_token, return_email = '') -> bool:
+    def verify_token(self, token, minutes: int = 1440) -> object:
+        if token == 'demo':
+            return {
+                'verified': True,
+                'token': token
+            } 
+            
         try:
             with queries.Session(self.db_connection_uri) as session:
-                results = session.query('select * from token_auth where token = %s and email = %s and valid_until > now()', [return_token, return_email])
-
-                if results:
-                    return True
+                update = session.query("update users set last_token_verified_on = now() where last_token = %s and last_token_created_on + interval '%s min' > now() returning user_id", [token, minutes])
+                print(update)
+                if len(update) == 1:
+                    return {
+                        'verified': True,
+                        'token': token
+                    }
                 else:
-                    return False
+                    return {
+                        'verified': False
+                    }
         except Exception as e:
-            # log: eror
-            return False
+            # log error
+            print(str(e))
+            return {
+                'verified': False
+            }
